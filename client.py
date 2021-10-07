9fb0216e7b4e9eb9b644be3d3cab8c1dda6bad20
nastyamak <90025016+nastyamak@users.noreply.github.com>
2021-10-07 17:29:07 +0300

    update Client.py

--- client.py
+++ client.py
@@ -1,16 +1,45 @@
 import socket
-from time import sleep
+import re
+from threading import *
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.setblocking(True)
+isOpen = False
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
 
-data = sock.recv(1024)
+def client():
+    global isOpen
+    adr = str(input('Введите адрес сервера = '))
+    if re.search(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", adr):
+        port = int(input('Введите порт сервера = '))
+        if 1024 < port < 65535:
+            sock.connect((adr, port))
+            isOpen = True
+            print("Подключение установленно")
+            while True:
+                msg = input()
+                sock.send(msg.encode())
 
-sock.close()
+                if msg == "exit":
+                    break
+        else:
+            print("Не верный Порт")
+    else:
+        print("Не верный IP")
 
-print(data.decode())
+
+def server():
+    global isOpen
+    while True:
+        if isOpen:
+            data = sock.recv(1024)
+            if data is not None or data != "":
+                print(data.decode())
+
+
+if __name__ == "__main__":
+    client = Thread(target=client)
+    server = Thread(target=server)
+
+    client.start()
+    server.start()
